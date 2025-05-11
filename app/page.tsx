98024900375e928bb0312f2bbb4c09074d0ef2db
Dione Santos <dionedev@gmail.com>
2025-05-11 13:07:09 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
       <h1 className="text-green-400">Hello World!!!</h1>
+      <Button>Saiba mais</Button>
     </div>
   );
 }
